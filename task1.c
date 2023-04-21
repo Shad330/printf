28114f42436dc47e9e9766be42b84099587bbbef
Aoub01 <ayyouboussouf@gmail.com>
2023-04-21 05:30:50 -0700

    task01

---
+++ task1.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int num = 26;
+    printf("%d\n", num);
+    printf("%i\n", num);
+    return (0);
+}
